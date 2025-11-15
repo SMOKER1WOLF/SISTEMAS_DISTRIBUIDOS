882ca644ffa7b3a69974e45fdb22d272e8f513f2
SMOKER1WOLF <elbraisito123@gmail.com>
2025-11-15 11:00:40 -0500

    C20

--- src/java/Herramientas/Conexion.java
+++ src/java/Herramientas/Conexion.java
@@ -12,6 +12,10 @@ public class Conexion {
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
 
+//                String url = System.getenv("DB_URL");
+//                String user = System.getenv("DB_USER");
+//                String pass = System.getenv("DB_PASS");
+
                 String url = "jdbc:mysql://centerbeam.proxy.rlwy.net:37177/railway?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
                 String user = "root";
                 String pass = "NfiRUMprCyZhzdxvLDtllIpgIapWVwaU";
--- src/java/Herramientas/Test.java
+++ src/java/Herramientas/Test.java
@@ -4,7 +4,6 @@ public class Test {
 
     public static void main(String[] args) {
         
-        
         Herramientas.Conexion.establecerConexion();
         Herramientas.Conexion.disconnect();
     }
